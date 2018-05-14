f928cced953f38e4c27f9102681443a6e1bc480c
Mangala Kodagoda <mangalak@peercore.com.au>
2018-05-15 06:23:22 +1000

    Added level1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Level1 from './Level1';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Level1 />, document.getElementById('root'));
 registerServiceWorker();
