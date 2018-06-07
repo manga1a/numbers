6e162ca114700da0a0cd00df494226795055103e
Mangala Kodagoda <mail.mangala@gmail.com>
2018-06-08 06:35:44 +1000

    Number input functionality

--- src/Level2.js
+++ src/Level2.js
@@ -22,7 +22,8 @@ class ButtonGrid extends Component {
 
   onBtnClick(e) {
     //const num = parseInt(e.target.id, 10);
-    console.log('clicked: ' + e.target.id)
+    //console.log('clicked: ' + e.target.id)
+    this.props.onClick(e.target.id);
   }
 
   render() {
@@ -87,36 +88,24 @@ function Card(props) {
 }
 
 //------------------------------------------------------
-function NumberCell(focus) {
-  return (
-    <div className="ui input focus">
-      <input type="text"
-        style={{width: 40 + 'px'}}
-        placeholder='7'
-      />
-    </div>
-  );
-}
+class InputCell extends Component {
+  constructor(props) {
+    super(props);
+    this.width = props.expected.length > 1 ? 50 : 40;
+    this.placeholder = props.expected.length > 1 ? '??' : '?';
+  }
 
-class NumberInput extends Component {
-      /*<div className="ui center aligned">
-          <div className="ui input focus">
-            <input type="text"
-              style={{width: 40 + 'px'}}
-              placeholder='7'
-            />
-          <div className="ui input">
-            <input type="text"
-              style={{width: 50 + 'px'}}
-              placeholder='88'
-            />
-      </div>*/
-  render() {
+  render () {
+    const focus = this.props.isFocus ? 'focus' : '';
     return (
-      <div className="ui two column centered grid">
-        <div className="column center aligned">
-          <NumberCell />
-        </div>
+      <div className={`ui large input ${focus}`}>
+        <input type="text"
+          placeholder={this.placeholder}
+          value={this.props.value}
+          style={{
+            width: this.width + 'px',
+            padding: '12px 12px'}}
+        />
       </div>
     );
   }
@@ -127,17 +116,61 @@ class NumberInput extends Component {
 class Recall extends Component {
   constructor(props) {
     super(props);
-    this.state = {val: ''};
+    this.onSelect = this.onSelect.bind(this);
+    this.state = {
+      focus: 0,
+      values: props.numbers.map(v => ''),
+    };
+
+    console.log(props.numbers);
+  }
+
+  onSelect(n) {
+    const idx = this.state.focus;
+    const expected = this.props.numbers[idx];
+    const actual = this.state.values[idx] + n;
+    const newValues = this.state.values.map(
+        (v, i) => i === idx ? actual : v
+      );
+
+    if(actual.length < expected.length) {
+      // update text only
+      this.setState({values: newValues});
+    } else {
+      //validate
+      /*
+      const newPassed = this.state.passed.map( (v, i) => {
+          return (i === this.state.focus ? (actual === expected) : v);
+      });*/
+      //console.log(newPassed);
+      //focus next
+      const newFocus = this.state.focus + 1;
+      //TODO: check for completion
+      this.setState({
+        focus: newFocus,
+        values: newValues,
+      });
+    }
+    //console.log(newVal, this.props.numbers[this.state.focus]);
   }
 
   render() {
-    console.log(this.props.numbers);
-
+    var inputCells = this.props.numbers.map( (val, idx) => {
+        return (<InputCell
+          expected={val}
+          isFocus={idx === this.state.focus}
+          value={this.state.values[idx]}
+          key={idx}/>);
+    });
     return (
       <div>
-        <NumberInput />
+        <div className="ui two column centered grid">
+          <div className="column center aligned">
+            {inputCells}
+          </div>
+        </div>
         <div className="ui grid">
-          <ButtonGrid />
+          <ButtonGrid onClick={this.onSelect}/>
         </div>
       </div>
     );
@@ -189,7 +222,7 @@ class Level2 extends Component {
   constructor(props){
     super(props);
     const numbers = Helpers.shuffleArray(Object.keys(Major.system));
-    const setSize = Math.min(5, numbers.length);
+    const setSize = Math.min(3, numbers.length);
     this.state = {
       currentSet: numbers.slice(0, setSize),
       bucket0: numbers.slice(setSize),
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// eslint-disable-next-line
 import App from './App';
+// eslint-disable-next-line
 import Level1 from './Level1';
 import Level2 from './Level2';
 import registerServiceWorker from './registerServiceWorker';
