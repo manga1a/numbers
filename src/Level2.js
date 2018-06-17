fa66396d5b860722712ff44f8eda105e6348b2ed
Mangala Kodagoda <mail.mangala@gmail.com>
2018-06-17 15:59:11 +1000

    Start new iteration for Level2

--- src/Level2.js
+++ src/Level2.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Major from './Major'
 import Helpers from './Helpers'
 
@@ -127,8 +128,56 @@ class InputCell extends Component {
 }
 
 //------------------------------------------------------
+// Show sequence of cards with numbers [and pegs]
+class Memorize extends Component {
+  static propTypes = {
+    interval: PropTypes.number,
+    numbers: PropTypes.array,
+    onComplete: PropTypes.func.isRequired,
+    showPeg: PropTypes.bool.isRequired,
+  };
+
+  constructor(props){
+    super(props);
+    this.state = {idx: 0};
+    this.onTrigger = this.onTrigger.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.onTrigger(), this.props.interval);
+  }
+
+  componentWillUnmount() {
+  }
+
+  onTrigger() {
+    const newIdx = this.state.idx + 1;
+    const interval = this.props.interval;
+    if(newIdx < this.props.numbers.length){
+      this.setState({idx: newIdx});
+      setTimeout(() => this.onTrigger(), interval);
+    } else {
+      this.props.onComplete();
+    }
+  }
+
+  render() {
+    const number = this.props.numbers[this.state.idx];
+    const peg = Major.System[number];
+    return (
+      <Card number={number} peg={peg} showPeg={this.props.showPeg}/>
+    );
+  }
+}
+
+//------------------------------------------------------
 // Recall number memory
 class Recall extends Component {
+  static propTypes = {
+    numbers: PropTypes.array,
+    onComplete: PropTypes.func.isRequired,
+  };
+
   constructor(props) {
     super(props);
     this.onSelect = this.onSelect.bind(this);
@@ -137,8 +186,6 @@ class Recall extends Component {
       focus: 0,
       values: props.numbers.map(v => ''),
     };
-
-    //console.log(props.numbers);
   }
 
   onSelect(n) {
@@ -211,267 +258,57 @@ class Recall extends Component {
 }
 
 //------------------------------------------------------
-// Show sequence of cards with numbers and pegs
-class FlashCards extends Component {
+// Practice memorization and recollection
+const PracticeMode = { Memorize: 0, Recall: 1};
+
+class Practice extends Component {
+  static propTypes = {
+    numbers: PropTypes.array,
+    onComplete: PropTypes.func.isRequired,
+  }
+
   constructor(props){
     super(props);
-    this.state = {idx: 0};
-    this.onTrigger = this.onTrigger.bind(this);
+    this.state = {mode: PracticeMode.Memorize}
+    this.goToRecall = this.goToRecall.bind(this);
   }
 
-  componentDidMount() {
-    setTimeout(() => this.onTrigger(), this.props.interval);
+  goToRecall() {
+    this.setState({mode: PracticeMode.Recall});
   }
 
-  componentWillUnmount() {
-  }
-
-  onTrigger() {
-    const newIdx = this.state.idx + 1;
-    const interval = this.props.interval;
-    if(newIdx < this.props.numbers.length){
-      this.setState({idx: newIdx});
-      setTimeout(() => this.onTrigger(), interval);
+  render () {
+    if(this.state.mode === PracticeMode.Memorize) {
+      return (
+        <Memorize
+          numbers={this.props.numbers}
+          interval={2000}
+          showPeg={true}
+          onComplete={this.goToRecall}
+        />
+      );
     } else {
-      this.props.onComplete();
+      return (
+        <Recall
+          numbers={this.props.numbers}
+          onComplete={this.props.onComplete}
+        />
+      );
     }
   }
-
-  render() {
-    const number = this.props.numbers[this.state.idx];
-    const peg = Major.System[number];
-    return (
-      <Card number={number} peg={peg} showPeg={this.props.showPeg}/>
-    );
-  }
-}
-
-//------------------------------------------------------
-function BucketSize(props) {
-  return (
-    <div className="ui three column centered grid">
-      <div className="column center aligned">
-        <div className="ui secondary segment">
-          <p>[[{props.picked}]]</p>
-          <p>
-            [{props.bucket0}] ==> [{props.bucket1}] ==> [{props.bucket2}]
-          </p>
-        </div>
-      </div>
-    </div>
-  );
 }
 
 //------------------------------------------------------
 // Root component
-const FLASH = 0;
-const RECALL = 1;
-const BUCKET_CONFIG = {
-  0: {showPeg: true, interval: 2000, pickCount: 4},
-  1: {showPeg: false, interval: 2000, pickCount: 4},
-  2: {showPeg: false, interval: 1200, pickCount: 4},
-};
-const STORE = 'Level2';
-
 class Level2 extends Component {
-  constructor(props){
-    super(props);
-    //load from localStorage
-    this.state = {
-      bucket0: [],
-      bucket1: [],
-      bucket2: [],
-      wins: [],
-      retired: [],
-      picked: [],
-      remainder: [],
-      mode: FLASH,
-      bucketId: 0,
-      setId: -1, //TODO: state gets reset???
-    };
-
-    this.fillBuckets = this.fillBuckets.bind(this);
-    this.pickNewSet = this.pickNewSet.bind(this);
-    this.reloadGame = this.reloadGame.bind(this);
-    this.goToRecall = this.goToRecall.bind(this);
-  }
-
-  componentDidMount() {
-    this.setState(prevState => {
-      const storedState = Helpers.loadState(STORE);
-      if(storedState) {
-        return storedState;
-      } else {
-        return this.reloadGame(prevState);
-      }
-    });
-  }
-
-  fillBuckets(pass, fail) {
-    this.setState(prevState => {
-      // always add failed items to bucket-0 (i.e. Leitner system)
-      const failState = { bucket0: prevState.bucket0.concat(fail)};
-      var newState;
-      if(prevState.bucketId === 0) {
-        newState = Object.assign(failState, {
-          bucket1: prevState.bucket1.concat(pass),
-        });
-      } else if(prevState.bucketId === 1) {
-        newState = Object.assign(failState, {
-          bucket2: prevState.bucket2.concat(pass),
-        });
-      } else {
-        newState = Object.assign(failState, {
-          wins: prevState.wins.concat(pass),
-        });
-      }
-      return newState;
-    });
-    // get new set from selected
-    this.pickNewSet();
-  }
-
-  pickNewSet() {
-    this.setState(prevState => {
-      if(0 < prevState.remainder.length) {
-        var pickCount = BUCKET_CONFIG[prevState.bucketId].pickCount;
-        pickCount = Math.min(pickCount, prevState.remainder.length);
-        return {
-            picked: prevState.remainder.slice(0, pickCount),
-            remainder: prevState.remainder.slice(pickCount),
-            mode: FLASH,
-        }
-      } else {
-        //save to local storage
-        const newGameState = this.reloadGame(prevState);
-        Helpers.saveState(STORE, newGameState);
-        return newGameState;
-      }
-    });
-  }
-
-  reloadGame(currentState) {
-    var newState = {};
-    var pickCount;
-    // if bucket-0 is not too low...
-    // no value in re-playing for 2 or less items
-    if(2 < currentState.bucket0.length) {
-      // fill picked, remainder from bucket-0
-      console.log('*** Load bucket-0');
-      debugger;
-      pickCount = Math.min(BUCKET_CONFIG[0].pickCount,
-        currentState.bucket0.length);
-
-      newState.remainder = Helpers.shuffleArray(
-        currentState.bucket0.slice());
-
-      newState.bucket0 = [];
-      newState.bucketId = 0;
-    }
-    //if bucket-1 is not empty...
-    else if(0 < currentState.bucket1.length) {
-      // fill picked, remainder from bucket-1
-      console.log('*** Load bucket-1');
-      debugger;
-      pickCount = Math.min(BUCKET_CONFIG[1].pickCount,
-        currentState.bucket1.length);
-
-      newState.remainder = Helpers.shuffleArray(
-        currentState.bucket1.slice());
-
-      newState.bucket1 = [];
-      newState.bucketId = 1;
-    }
-    //if bucket-2 is not empty...
-    else if(0 < currentState.bucket2.length) {
-      // fill picked, remainder from bucket-2
-      console.log('*** Load bucket-2');
-      debugger;
-
-      let newSet = currentState.bucket2.slice();
-      newState.bucket2 = [];
-      // add from bucket-3 if any...
-      //... only if previous bucket is not 2
-      //... and this is a replay
-      if(currentState.bucketId !== 2) {
-        newSet = newSet.concat(currentState.wins);
-        newState.wins = [];
-      }
-
-      pickCount = Math.min(BUCKET_CONFIG[2].pickCount, newSet.length);
-
-      newState.remainder = Helpers.shuffleArray(newSet);
-
-      newState.bucketId = 2;
-    }
-    // all buckets are empty...
-    else {
-      const newSetId = currentState.setId + 1;
-      console.log(`*** Load new set ${newSetId}`);
-      debugger;
-
-      // load new set
-      if(newSetId < Major.PlaySet.length) {
-        let newSet = Major.PlaySet[newSetId].slice();
-        // add from bucket-0 if any
-        newSet = newSet.concat(currentState.bucket0);
-        newState.bucket0 = [];
-        // new set starts with bucket-0
-        pickCount = Math.min(BUCKET_CONFIG[0].pickCount, newSet.length);
-
-        // promote bucket-3
-        newState.retired = currentState.retired.concat(currentState.wins);
-        newState.wins = [];
-
-        newState.remainder = Helpers.shuffleArray(newSet);
-        newState.setId = newSetId;
-        newState.bucketId = 0;
-
-      } else {
-        console.log('***** END OF LEVEL-2 *****');
-      }
-    }
-
-    newState.picked = newState.remainder.slice(0, pickCount);
-    newState.remainder = newState.remainder.slice(pickCount);
-    newState.mode = FLASH;
-
-    return newState;
-  }
-
-  goToRecall() {
-    this.setState({mode: RECALL});
-  }
-
   render() {
-    const flashOrRecall = this.state.mode === FLASH ?
-      (
-        <FlashCards
-          numbers={this.state.picked}
-          interval={BUCKET_CONFIG[this.state.bucketId].interval}
-          showPeg={BUCKET_CONFIG[this.state.bucketId].showPeg}
-          onComplete={this.goToRecall}
-        />
-      ) :
-      (
-        <Recall
-          numbers={this.state.picked}
-          onComplete={this.fillBuckets}
-        />
-      );
-
     return(
       <div className="ui container">
-        {flashOrRecall}
-
-        <BucketSize
-          picked={
-            this.state.remainder.length +
-            this.state.picked.length
-          }
-          bucket0={this.state.bucket0 ? this.state.bucket0.length : 0}
-          bucket1={this.state.bucket1 ? this.state.bucket1.length : 0}
-          bucket2={this.state.bucket2 ? this.state.bucket2.length : 0}
+        <Practice
+          numbers={['00', '02', '01', '03']}
+          onComplete={(pass, fail) => {
+            console.log(pass, fail);
+          }}
         />
       </div>
     );
