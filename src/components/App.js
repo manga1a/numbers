54eb99e2685c74f9056e47f3bd16ae322d7f585d
Mangala Kodagoda <mail.mangala@gmail.com>
2018-08-25 06:41:52 +1000

    Add react-router support

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import LevelOne from './levels/Level1';
-import LevelTwo from './levels/Level2';
-import LevelThree from './levels/Level3';
+//<a className="active item"><Link to="/level-one">Level One</Link></a>
 
 class App extends Component {
-  render(){
+  render() {
     return (
-      <LevelThree />
+      <div className="ui grid">
+        <div className="four wide column">
+          <div className="ui vertical fluid tabular menu">
+            <div className="item"><Link to="/level-one">Level One</Link></div>
+            <div className="item"><Link to="/level-two">Level Two</Link></div>
+            <div className="item"><Link to="/level-three">Level Three</Link></div>
+          </div>
+        </div>
+        <div className="twelve wide stretched column">
+          <div className="ui segment">
+            {this.props.children}
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+//import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+//import {Router, Route} from 'react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './index.css';
-// eslint-disable-next-line
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />,
-  document.getElementById('content'));
+import App from './components/App';
+import LevelOne from './components/levels/Level1';
+import LevelTwo from './components/levels/Level2';
+import LevelThree from './components/levels/Level3';
+
+render(
+    <BrowserRouter>
+      <App>
+        <Route path="/level-one" component={LevelOne}/>
+        <Route path="/level-two" component={LevelTwo}/>
+        <Route path="/level-three" component={LevelThree}/>
+      </App>
+    </BrowserRouter>,
+    document.getElementById('content')
+);
+
 registerServiceWorker();
